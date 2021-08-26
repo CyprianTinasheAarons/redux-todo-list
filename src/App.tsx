b2eba86002742b784a30434846d175f8eb419277
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2021-08-26 14:40:18 +0200

    Redux Toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+// Material-UI Imports
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+// Other Imports
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "./redux/store";
+import { addTodo, removeTodo, setTodoStatus } from "./redux/todosSlice";
+
 
 function App() {
+  //React Hooks
+  const [todoDescription, setTodoDescription] = useState("");
+  
+  // Redux Hooks
+  const dispatch = useDispatch<AppDispatch>();
+  const todoList = useSelector((state: RootState) => state);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="xs">
+      <Typography variant="h3" align="center">
+        Todo List
+      </Typography>
+      <TextField
+        label="Todo Description"
+        value={todoDescription}
+        onChange={(e) => setTodoDescription(e.target.value)}
+        margin="normal"
+        fullWidth
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          dispatch(addTodo(todoDescription));
+          setTodoDescription("");
+        }}
+      >
+        Add Todo
+      </Button>
+      <List>
+        {todoList.map((todo) => (
+             <ListItem key={todo.id}>
+            <ListItemText
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+            >
+              {todo.description}
+            </ListItemText>
+            <ListItemSecondaryAction>
+              <IconButton
+                onClick={() => {
+                  dispatch(removeTodo(todo.id));
+                }}
+              >
+                <DeleteIcon />
+              </IconButton>
+              <Checkbox
+                edge="end"
+                value={todo.completed}
+                onChange={() => {
+                  dispatch(
+                    setTodoStatus({ completed: !todo.completed, id: todo.id })
+                  );
+                }}
+              />
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+      </List>
+
+
+    </Container>
   );
 }
 
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+interface Todo {
+    id: string
+    description: string
+    completed: boolean
+}
